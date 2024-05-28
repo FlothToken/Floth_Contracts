71ecf089b02d4b4611c40a16e5527fcdd43e229b
RemoteJake <jakecrocker83@gmail.com>
2024-05-28 13:47:51 -0600

    Floth contract cleanup + buy/sell tax.

--- contracts/Floth.sol
+++ contracts/Floth.sol
@@ -5,82 +5,90 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract Floth is ERC20Votes {
 
-    //Initial taxes for bots.
-    uint256 public initialBuyTax = 25;
-    uint256 public initialSellTax = 35;
+    //Bot tax
+    uint256 public buyTax = 25;
+    uint256 public sellTax = 35;
 
-    //Post-10 min taxes.
-    uint256 public finalBuyTax = 0; 
-    uint256 public finalSellTax = 3;
-
-    //Bot tax time trackers.
-    uint256 public taxPeriod = 10 minutes;
+    address private admin;
     uint256 public deploymentTime;
 
-    //Tracks if transaction is a buy or a sell.
-    bool private isBuy = false;
-    bool private isSell = false;
+    // Store DEX addresses to calculate if buy/sell/transfer.
+    mapping(address => bool) public dexAddresses;
 
     address public grantFundWallet = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Update to actual wallet.
     address public lpPairAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Update to actual address.
 
-    constructor() ERC20("Floth", "FLOTH") {
+    constructor(address[] memory _dexAddresses) ERC20("Floth", "FLOTH") {
         _mint(msg.sender, 1000000 * 10 ** 18);
         deploymentTime = block.timestamp;
+        admin = msg.sender;
+
+        for (uint256 i = 0; i < _dexAddresses.length; i++) {
+            dexAddresses[_dexAddresses[i]] = true;
+        }
     }
 
-    //Gets the tax rates based on current time.
-     function getTaxRates() public view returns (uint256 buyTax, uint256 sellTax) {
-        //If less than 10 minutes have passed, use 25% buy / 35% sell tax.
-        if (block.timestamp <= deploymentTime + taxPeriod) {
-            return (initialBuyTax, initialSellTax);
-        } else {
-        //If 10 mins have passed, use normal tax rates.
-            return (finalBuyTax, finalSellTax);
+    modifier onlyAdmin(){
+        if(msg.sender != admin){
+            revert("You must be admin.");
         }
+        _;
+    }
+
+    //Set sell bot tax.
+    function setSellBotTax(uint256 _newSellTax) external onlyAdmin {
+        sellTax = _newSellTax;
+    }
+
+    //Set buy bot tax.
+    function setBuyBotTax(uint256 _newBuyTax) external onlyAdmin {
+        buyTax = _newBuyTax;
+    }
+
+    //Add DEX address to mapping.
+    function addDexAddress(address _dexAddress) external onlyOwner {
+        dexAddresses[_dexAddress] = true;
+    }
+
+    //Remove DEX address to mapping.
+    function removeDexAddress(address _dexAddress) external onlyOwner {
+        dexAddresses[_dexAddress] = false;
     }
 
     //Transfer tokens with/without tax, based on time of buy/sell.
-    function _transfer(address _from, address _to, uint256 _amount) internal override {
-        require(_to != address(this), "BeamToken._transfer: transfer to self not allowed");
+    function _transfer(address _sender, address _recipient, uint256 _amount) internal override {
+        require(_recipient != address(this), "Contract sending tokens to itself is not allowed.");
+        require(_recipient != msg.sender, "FlothToken._transfer: transfer to self not allowed");
 
         uint256 taxAmount = 0; //Amount to be taxed for this transaction.
-        (uint256 buyTax, uint256 sellTax) = getTaxRates(); //Get current tax rates.
 
         //Case for if it's a buy transaction.
-        if(_from == address(this) && _to != address(0)){
-            isBuy = true;
+        if(dexAddresses[_sender]){
             taxAmount = _amount * (buyTax / 100); //Amount * buy tax as a decimal.
+            if(taxAmount > 0){
+                super._transfer(_from, grantFundWallet, taxAmount);
+            }
         }
         //Case for if it's a sell transaction.
-        else if(_from != address(0) && _to == address(this)){
-            isSell = true;
+        else if(dexAddresses[_recipient]){
             taxAmount = _amount * (sellTax / 100); //Amount * sell tax as a decimal.
+
+            if(taxAmount > 0){
+                uint256 grantFundAmount = _taxAmount * (833 / 1000); //83.3% (2.5% from the 3%)
+                int256 lpPairingAmount = _taxAmount - grantFundAmount; //16.3% (0.5% from the 3%)
+
+                super._transfer(_from, grantFundWallet, grantFundAmount);
+                
+                //Also send to the LP Pairing until 10% LP allocation reserve is depleted.
+            }
+        }
+        //Case for if it's a wallet to wallet transfer.
+        else{
+        
         }
 
         uint256 totalPayable = _amount - taxAmount; //Final tax amount is deducted.
 
-        super._transfer(_from, _to, totalPayable);
-
-        if (taxAmount > 0) {
-            _distributeTax(_from, taxAmount);
-        }
+        super._transfer(_sender, _recipient, totalPayable);
     }
-
-    //Distributes the buy/sell tax.
-    function _distributeTax(address _from, uint256 taxAmount) internal{
-         if(isBuy){
-            //Need to ask about what to do with 25% buy tax in initial 10 mins.
-        }
-        else if(isSell){
-            uint256 grantFundAmount = taxAmount * (833 / 1000); //83.3% (2.5% from the 3%)
-            uint256 lpPairingAmount = taxAmount - grantFundAmount; //16.3% (0.5% from the 3%)
-
-            super._transfer(_from, grantFundWallet, grantFundAmount);
-            
-            //Also send to the LP Pairing until 10% LP allocation reserve is depleted.
-        }
-    }
-
-
 }
