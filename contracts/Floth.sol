7c0e4dde656327b89ce390e197dd00519e0b94e3
Westy <ben.westcott@gmail.com>
2024-05-30 19:52:16 +0100

    Commented out floth just for compilation

--- contracts/Floth.sol
+++ contracts/Floth.sol
@@ -1,99 +1,104 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
+// contract Floth is ERC20Votes, Ownable {
+//     //Bot tax
+//     uint256 public buyTax = 25;
+//     uint256 public sellTax = 35;
 
-contract Floth is ERC20Votes, Ownable {
+//     uint256 public deploymentTime;
 
-    //Bot tax
-    uint256 public buyTax = 25;
-    uint256 public sellTax = 35;
+//     // Store DEX addresses to calculate if buy/sell/transfer.
+//     mapping(address => bool) public dexAddresses;
 
-    uint256 public deploymentTime;
+//     address public grantFundWallet = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Update to actual wallet.
+//     address public lpPairAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Update to actual address.
 
-    // Store DEX addresses to calculate if buy/sell/transfer.
-    mapping(address => bool) public dexAddresses;
+//     event TaxUpdated(string taxType, uint256 newTax);
+//     event DexAddressUpdated(address dexAddress, bool isAdded);
 
-    address public grantFundWallet = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Update to actual wallet.
-    address public lpPairAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Update to actual address.
+//     error InvalidTaxAmount();
 
-    event TaxUpdated(string taxType, uint256 newTax);
-    event DexAddressUpdated(address dexAddress, bool isAdded);
+//     constructor(address[] memory _dexAddresses) ERC20("Floth", "FLOTH") {
+//         _mint(msg.sender, 1000000 * 10 ** 18);
+//         deploymentTime = block.timestamp;
 
-    error InvalidTaxAmount();
+//         for (uint256 i = 0; i < _dexAddresses.length; i++) {
+//             dexAddresses[_dexAddresses[i]] = true;
+//         }
+//     }
 
-    constructor(address[] memory _dexAddresses) ERC20("Floth", "FLOTH") {
-        _mint(msg.sender, 1000000 * 10 ** 18);
-        deploymentTime = block.timestamp;
+//     //Set sell bot tax.
+//     function setSellBotTax(uint256 _newSellTax) external onlyOwner {
+//         //Sell tax cannot be more than 5%.
+//         if (_newSellTax > 5) {
+//             revert InvalidTaxAmount();
+//         }
+//         sellTax = _newSellTax;
+//         emit TaxUpdated("Sell", _newSellTax);
+//     }
 
-        for (uint256 i = 0; i < _dexAddresses.length; i++) {
-            dexAddresses[_dexAddresses[i]] = true;
-        }
-    }
+//     //Set buy bot tax.
+//     function setBuyBotTax(uint256 _newBuyTax) external onlyOwner {
+//         //Buy tax cannot be more than 5%.
+//         if (_newBuyTax > 5) {
+//             revert InvalidTaxAmount();
+//         }
+//         buyTax = _newBuyTax;
+//         emit TaxUpdated("Buy", _newBuyTax);
+//     }
 
-    //Set sell bot tax.
-    function setSellBotTax(uint256 _newSellTax) external onlyOwner {
-        //Sell tax cannot be more than 5%.
-        if(_newSellTax > 5){
-            revert InvalidTaxAmount();
-        }
-        sellTax = _newSellTax;
-        emit TaxUpdated("Sell", _newSellTax);
-    }
+//     //Add DEX address to mapping.
+//     function addDexAddress(address _dexAddress) external onlyOwner {
+//         dexAddresses[_dexAddress] = true;
+//         emit DexAddressUpdated(_dexAddress, true);
+//     }
 
-    //Set buy bot tax.
-    function setBuyBotTax(uint256 _newBuyTax) external onlyOwner {
-        //Buy tax cannot be more than 5%.
-        if(_newBuyTax > 5){
-            revert InvalidTaxAmount();
-        }
-        buyTax = _newBuyTax;
-        emit TaxUpdated("Buy", _newBuyTax);
-    }
+//     //Remove DEX address to mapping.
+//     function removeDexAddress(address _dexAddress) external onlyOwner {
+//         dexAddresses[_dexAddress] = false;
+//         emit DexAddressUpdated(_dexAddress, false);
+//     }
 
-    //Add DEX address to mapping.
-    function addDexAddress(address _dexAddress) external onlyOwner {
-        dexAddresses[_dexAddress] = true;
-        emit DexAddressUpdated(_dexAddress, true);
-    }
+//     //Transfer tokens with/without tax, based on time of buy/sell.
+//     function _transfer(
+//         address _sender,
+//         address _recipient,
+//         uint256 _amount
+//     ) internal override {
+//         require(
+//             _recipient != address(this),
+//             "Contract sending tokens to itself is not allowed."
+//         );
 
-    //Remove DEX address to mapping.
-    function removeDexAddress(address _dexAddress) external onlyOwner {
-        dexAddresses[_dexAddress] = false;
-        emit DexAddressUpdated(_dexAddress, false);
-    }
+//         uint256 taxAmount = 0; //Amount to be taxed for this transaction.
 
-    //Transfer tokens with/without tax, based on time of buy/sell.
-    function _transfer(address _sender, address _recipient, uint256 _amount) internal override {
-        require(_recipient != address(this), "Contract sending tokens to itself is not allowed.");
+//         //Case for if it's a buy transaction.
+//         if (dexAddresses[_sender]) {
+//             taxAmount = _amount * (buyTax / 100); //Amount * buy tax as a decimal.
+//             if (taxAmount > 0) {
+//                 super._transfer(_sender, grantFundWallet, taxAmount);
+//             }
+//         }
+//         //Case for if it's a sell transaction.
+//         if (dexAddresses[_recipient]) {
+//             taxAmount = _amount * (sellTax / 100); //Amount * sell tax as a decimal.
 
-        uint256 taxAmount = 0; //Amount to be taxed for this transaction.
+//             if (taxAmount > 0) {
+//                 uint256 grantFundAmount = _taxAmount * (833 / 1000); //83.3% (2.5% from the 3%)
+//                 int256 lpPairingAmount = _taxAmount - grantFundAmount; //16.7% (0.5% from the 3%)
 
-        //Case for if it's a buy transaction.
-        if(dexAddresses[_sender]){
-            taxAmount = _amount * (buyTax / 100); //Amount * buy tax as a decimal.
-            if(taxAmount > 0){
-                super._transfer(_sender, grantFundWallet, taxAmount);
-            }
-        }
-        //Case for if it's a sell transaction.
-        if(dexAddresses[_recipient]){
-            taxAmount = _amount * (sellTax / 100); //Amount * sell tax as a decimal.
+//                 super._transfer(_sender, grantFundWallet, grantFundAmount);
 
-            if(taxAmount > 0){
-                uint256 grantFundAmount = _taxAmount * (833 / 1000); //83.3% (2.5% from the 3%)
-                int256 lpPairingAmount = _taxAmount - grantFundAmount; //16.7% (0.5% from the 3%)
+//                 //Also send to the LP Pairing until 10% LP allocation reserve is depleted. REVIEW.
+//             }
+//         }
 
-                super._transfer(_sender, grantFundWallet, grantFundAmount);
+//         uint256 totalPayable = _amount - taxAmount; //Final tax amount is deducted.
 
-                //Also send to the LP Pairing until 10% LP allocation reserve is depleted. REVIEW.
-            }
-        }
-
-        uint256 totalPayable = _amount - taxAmount; //Final tax amount is deducted.
-
-        super._transfer(_sender, _recipient, totalPayable);
-    }
-}
+//         super._transfer(_sender, _recipient, totalPayable);
+//     }
+// }
