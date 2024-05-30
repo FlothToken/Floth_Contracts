c13f2e0d3a74a22e555930960ae5249a06f8b084
RemoteJake <jakecrocker83@gmail.com>
2024-05-30 16:06:47 -0600

    test

--- contracts/pFloth.sol
+++ contracts/pFloth.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
+//mandem
 contract pFLOTH is ERC20, Ownable, ReentrancyGuard {
     uint256 public constant MAX_SUPPLY = 30 * 10 ** 9 * 10 ** 18; // 30 billion pFLOTH
     uint256 public constant EXCHANGE_RATE = 10000; // 1 FLR = 10,000 pFLOTH
