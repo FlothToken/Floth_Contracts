7b5173176c637e02884045853b4d19c8f5242f4f
Westy <ben.westcott@gmail.com>
2024-06-05 22:22:19 +0100

    Fixed syntax errors in tests

--- test/pFloth.js
+++ test/pFloth.js
@@ -7,7 +7,7 @@ describe("pFLOTH Contract", function () {
   let addr1;
   let addr2;
   const PRESALE_DURATION = 3600; // 1 hour
-  const EXCHANGE_RATE = 10000;
+  const EXCHANGE_RATE = 10000n;
   const MAX_SUPPLY = ethers.parseUnits("30000000000", 18); // 30 billion pFLOTH
   const WALLET_LIMIT = ethers.parseUnits("2500000000", 18); // 2.5 billion pFLOTH
 
@@ -28,15 +28,23 @@ describe("pFLOTH Contract", function () {
 
   describe("Presale", function () {
     it("Should revert if presale has ended", async function () {
-      await ethers.provider.send("evm_increaseTime", [PRESALE_DURATION + 1]);
-      await ethers.provider.send("evm_mine");
+      const initialTimestamp = (await ethers.provider.getBlock("latest")).timestamp;
+      console.log("Initial Timestamp:", initialTimestamp);
 
+      // Increase time by the presale duration plus one second
+      await ethers.provider.send("evm_increaseTime", [PRESALE_DURATION + 1]);
+      await ethers.provider.send("evm_mine", []);
+
+      const newTimestamp = (await ethers.provider.getBlock("latest")).timestamp;
+      console.log("New Timestamp:", newTimestamp);
+
+      // Attempt to participate in the presale after the presale period has ended
       await expect(pFLOTH.connect(addr1).presale({ value: ethers.parseUnits("1", 18) })).to.be.revertedWith("PresaleEnded");
     });
 
     it("Should mint the correct amount of pFLOTH tokens", async function () {
       const amountFLR = ethers.parseUnits("1", 18);
-      const amountpFLOTH = amountFLR.mul(EXCHANGE_RATE);
+      const amountpFLOTH = amountFLR * EXCHANGE_RATE;
 
       await pFLOTH.connect(addr1).presale({ value: amountFLR });
 
@@ -45,18 +53,18 @@ describe("pFLOTH Contract", function () {
     });
 
     it("Should revert if minting exceeds MAX_SUPPLY", async function () {
-      const amountFLR = MAX_SUPPLY.div(EXCHANGE_RATE).add(1);
+      const amountFLR = MAX_SUPPLY / EXCHANGE_RATE + 1n;
       await expect(pFLOTH.connect(addr1).presale({ value: amountFLR })).to.be.revertedWith("ExceedsSupply");
     });
 
     it("Should revert if minting exceeds WALLET_LIMIT", async function () {
-      const amountFLR = WALLET_LIMIT.div(EXCHANGE_RATE).add(1);
+      const amountFLR = WALLET_LIMIT / EXCHANGE_RATE + 1n;
       await expect(pFLOTH.connect(addr1).presale({ value: amountFLR })).to.be.revertedWith("WalletLimitExceeded");
     });
 
     it("Should emit Presale event", async function () {
       const amountFLR = ethers.parseUnits("1", 18);
-      const amountpFLOTH = amountFLR.mul(EXCHANGE_RATE);
+      const amountpFLOTH = amountFLR * EXCHANGE_RATE;
 
       await expect(pFLOTH.connect(addr1).presale({ value: amountFLR }))
         .to.emit(pFLOTH, "Presale")
