587faed4fa8c6776a4d03f7f81eba74632c77ba6
Westy <westy@ethereallabs.io>
2024-08-02 22:57:11 +0100

    :recycle: :sparkles: Rewrite of floth pass to accommodate votes!

--- contracts/FlothPass.sol
+++ contracts/FlothPass.sol
@@ -1,94 +1,91 @@
-// SPDX-License-Identifier: MIT  
-/**                           
- * @title Floth Pass
- * @author Ethereal Labs
- * @notice Floth Pass - a contract for minting a FLOTH Pass
- */
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
 import "./IFloth.sol";
-import "erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
-import "erc721a-upgradeable/contracts/extensions/ERC721AQueryableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721VotesUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
+/**
+ * @title FlothPass contract for minting Floth Pass NFTs.
+ * @author Ethereal Labs Ltd
+ * @notice This contract allows users to mint Floth Pass NFTs using FLOTH tokens.
+ */
 contract FlothPass is
-    ERC721AUpgradeable,
-    ERC721AQueryableUpgradeable,
+    ERC721VotesUpgradeable,
+    ERC721EnumerableUpgradeable,
     AccessControlUpgradeable,
     ReentrancyGuardUpgradeable
 {
-    //Setup variables arranged for optimised storage slots
-
-    //The address of the Floth contract
+    // Floth contract interface.
     IFloth public flothContract;
 
-    //Payable withdraw address
+    // Address to withdraw funds to.
     address payable public withdrawAddress;
 
-    //Floth vault address
+    // Address to send minted FLOTH to.
     address public flothVault;
 
-    //The collection base URI
+    // Base URI for token metadata.
     string public _currentBaseURI;
 
-    //Floth Initial Price
-    uint256 public price; // 1000 FLOTH
+    // Price to mint a single token.
+    uint256 public price;
 
-    //Price increment
+    // Price increment for every 10 tokens minted.
     uint256 public priceIncrement;
 
-    //Mints since last increment
+    // Number of tokens minted.
+    uint16 public numberMinted;
+
+    // Number of mints since last price increment.
     uint16 public mintsSinceLastIncrement;
 
-    //Max supply
+    // Maximum number of tokens that can be minted.
     uint16 public maxSupply;
 
-    //Public sale active flag
+    // Whether the sale is active.
     bool public saleActive;
 
     // Gap for upgradeability
     uint256[50] private __gap;
 
-    /**
-     * @dev To reduce smart contract size & gas usages, we are using custom errors rather than using require.
-     */
+    // Roles
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
+
+    // Events
     error SaleInactive();
     error InsufficientFunds();
     error InsufficientFundsInContract();
     error InsufficientRole();
     error ExceedsMaxSupply();
     error TransferFailed();
-    error CannotMintToZeroAddress();
-    error CannotDeployAsZeroAddress();
-
-    //Roles
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Initialize Functions
-    ///////////////////////////////////////////////////////////////////////////
+    error ZeroAddress();
 
     /**
-     * Initialize function for proxy.
+     *
+     * @dev Initialize function for proxy.
      * Calls the internal initialize function.
+     * @param flothContractAddress Address of the Floth contract.
      */
-    function initialize(address flothContractAddress) public initializerERC721A initializer {
-        __ERC721A_init("Floth Pass", "FPASS");
-        __ERC721AQueryable_init();
+    function initialize(address flothContractAddress) public initializer {
+        __ERC721_init("Floth Pass", "FPASS");
+        __ERC721Enumerable_init();
+        __ERC721Votes_init();
         __AccessControl_init();
+        __ReentrancyGuard_init();
         __FlothPass_init(flothContractAddress);
-
 
         _grantRole(ADMIN_ROLE, msg.sender);
         _setRoleAdmin(WITHDRAW_ROLE, ADMIN_ROLE);
-        _grantRole(ADMIN_ROLE, msg.sender);
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     /**
-     * Initialize function which sets the defaults for state variables
+     * @dev Initialize function which sets the defaults for state variables
+     * @param flothContractAddress Address of the Floth contract.
      */
     function __FlothPass_init(address flothContractAddress) internal initializer {
         _currentBaseURI = "";
@@ -108,10 +105,6 @@ contract FlothPass is
         _disableInitializers();
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-    // Mint Functions
-    ///////////////////////////////////////////////////////////////////////////
-
     /**
      * @dev Mint function to mint floth pass to the caller.
      * Requires the caller to have enough FLOTH to mint.
@@ -125,10 +118,8 @@ contract FlothPass is
             revert SaleInactive();
         }
 
-        uint256 totalMinted = _totalMinted();
-
-        // Check total minted against max supply
-        if (totalMinted + _quantity > maxSupply) {
+        // Check if the total minted plus the quantity is less than the max supply.
+        if (numberMinted + _quantity > maxSupply) {
             revert ExceedsMaxSupply();
         }
 
@@ -139,7 +130,7 @@ contract FlothPass is
         // @dev Note i = 1 not i = 0
         for (uint16 i = 1; i <= _quantity; i++) {
             totalPrice += currentPrice;
-            if ((totalMinted + i) % 10 == 0) {
+            if ((numberMinted + i) % 10 == 0) {
                 currentPrice += priceIncrement;
             }
         }
@@ -152,52 +143,35 @@ contract FlothPass is
         // Transfer the total price from the caller to the flothVault
         flothContract.transferFrom(msg.sender, flothVault, totalPrice);
 
-        // Mint the requested quantity to the caller
-        _safeMint(msg.sender, _quantity);
+        // Mint the quantity of tokens to the caller
+        for (uint16 i = 0; i < _quantity; i++) {
+            _safeMint(msg.sender, numberMinted + i);
+            numberMinted += 1;
+        }
 
-        // Update the number of mints since the last price increment
-        mintsSinceLastIncrement = (uint16(totalMinted) + _quantity) % 10;
+        mintsSinceLastIncrement = (numberMinted + _quantity) % 10;
 
-        price = currentPrice; // Update the price to the new current price
+        // Update the price to the new current price
+        price = currentPrice;
     }
-
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Withdraw Functions
-    ///////////////////////////////////////////////////////////////////////////
-
+    
     /**
-     * @dev Withdraws the balance of the contract to the withdraw address if it exists.
-     * If it does not exist, it withdraws to the caller.
-     * Checks if caller has withdraw role or admin role.
-     * Checks if withdraw address is valid.
+     * @dev Withdraw function to withdraw flare funds from the contract.
+     * Requires the caller to have the WITHDRAW_ROLE or ADMIN_ROLE.
      * @param _amount the amount to withdraw
-     * @param _withdrawAll if true, withdraws the entire balance of the contract
+     * @param _withdrawAll whether to withdraw all the funds
      */
-    function withdraw(
-        uint256 _amount,
-        bool _withdrawAll
-    ) external nonReentrant {
-        if (
-            !hasRole(WITHDRAW_ROLE, msg.sender) &&
-            !hasRole(ADMIN_ROLE, msg.sender)
-        ) {
+    function withdraw(uint256 _amount, bool _withdrawAll) external nonReentrant {
+        if (!hasRole(WITHDRAW_ROLE, msg.sender) && !hasRole(ADMIN_ROLE, msg.sender)) {
             revert InsufficientRole();
         }
 
-        uint256 amountToWithdraw = _withdrawAll
-            ? address(this).balance
-            : _amount;
-
-        // If _withdrawAll is false, then check if there are enough funds in the contract
+        uint256 amountToWithdraw = _withdrawAll ? address(this).balance : _amount;
         if (!_withdrawAll && amountToWithdraw > address(this).balance) {
             revert InsufficientFundsInContract();
         }
 
-        address payable recipient = withdrawAddress != address(0)
-            ? withdrawAddress
-            : payable(msg.sender);
-
+        address payable recipient = withdrawAddress != address(0) ? withdrawAddress : payable(msg.sender);
         _withdraw(recipient, amountToWithdraw);
     }
 
@@ -208,171 +182,177 @@ contract FlothPass is
      */
     function _withdraw(address _address, uint256 _amount) internal {
         (bool success, ) = _address.call{value: _amount}("");
-        if (!success){
+        if (!success) {
             revert TransferFailed();
         }
     }
 
     /**
-     * @dev Withdraws the FLOTH balance of the contract to the withdraw address if it exists.
-     * If it does not exist, it withdraws to the caller.
-     * Checks if caller has withdraw role or admin role.
-     * Checks if withdraw address is valid.
+     * @dev Withdraw FLOTH function to withdraw FLOTH from the contract.
      * @param _amount the amount to withdraw
-     * @param _withdrawAll if true, withdraws the entire balance of the contract
+     * @param _withdrawAll whether to withdraw all the funds
      */
     function withdrawFLOTH(uint256 _amount, bool _withdrawAll) external nonReentrant {
-        if (
-            !hasRole(WITHDRAW_ROLE, msg.sender) &&
-            !hasRole(ADMIN_ROLE, msg.sender)
-        ) {
+        if (!hasRole(WITHDRAW_ROLE, msg.sender) && !hasRole(ADMIN_ROLE, msg.sender)) {
             revert InsufficientRole();
         }
 
-        uint256 amountToWithdraw = _withdrawAll
-            ? flothContract.balanceOf(address(this))
-            : _amount;
-
-        // If _withdrawAll is false, then check if there are enough funds in the contract
+        uint256 amountToWithdraw = _withdrawAll ? flothContract.balanceOf(address(this)) : _amount;
         if (!_withdrawAll && amountToWithdraw > flothContract.balanceOf(address(this))) {
             revert InsufficientFundsInContract();
         }
 
-        address recipient = withdrawAddress != address(0)
-            ? withdrawAddress
-            : msg.sender;
-
+        address recipient = withdrawAddress != address(0) ? withdrawAddress : msg.sender;
         flothContract.transfer(recipient, amountToWithdraw);
     }
 
     /**
-     * @dev Gets the number of floth passes minted for an owner.
-     * @param _owner the owner of the floth passes to get the number minted for
+     * @dev Override for the tokenURI function to return the token URI
+     * @param _tokenId the token id to get the URI for
+     * @return the token URI
      */
-    function numberMinted(address _owner) public view returns (uint256) {
-        return _numberMinted(_owner);
-    }
-
-    /**
-     * @dev Function to obtain the uri/json file of a particular token id.
-     * @param _tokenId the token id to get the uri for
-     */
-    function tokenURI(uint256 _tokenId) public view virtual override(ERC721AUpgradeable, IERC721AUpgradeable)   returns (string memory)
-    {
+    function tokenURI(uint256 _tokenId) public view override(ERC721Upgradeable) returns (string memory) {
         return super.tokenURI(_tokenId);
     }
 
     /**
-     * @dev ERC721AUpgradeable internal function to set the starting token id.
+     * @dev Override for the supportsInterface function to return the supported interfaces
+     * @param interfaceId the interface id to check
+     * @return whether the interface is supported
      */
-    function _startTokenId() internal view virtual override returns (uint256) {
-        return 1;
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable) returns (bool) {
+        return ERC721EnumerableUpgradeable.supportsInterface(interfaceId) || AccessControlUpgradeable.supportsInterface(interfaceId);
     }
 
     /**
-     * @dev Used to identify the interfaces supported by this contract.
-     * @param interfaceId the interface id to check for support
+     * @dev Getter for the number of tokens minted
+     * @return the number of tokens minted
      */
-    function supportsInterface(bytes4 interfaceId) public view override
-    (ERC721AUpgradeable, IERC721AUpgradeable, AccessControlUpgradeable) returns (bool)
-    {
-        return ERC721AUpgradeable.supportsInterface(interfaceId) || 
-               AccessControlUpgradeable.supportsInterface(interfaceId);
+    function getNumberMinted() external view returns (uint16) {
+        return numberMinted;
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-    //#region State Changes
-    ///////////////////////////////////////////////////////////////////////////
-
     /**
-     * @dev Sets the sale active state of the contract
-     * @param _saleActive state to change to
-     * Only callable by admin
+     * @dev Setter for the sale active status
+     * @param _saleActive the new sale active status
      */
     function setSaleActive(bool _saleActive) external onlyRole(ADMIN_ROLE) {
         saleActive = _saleActive;
     }
 
     /**
-     * @dev Sets the public mint price per floth pass
-     * @param _newPrice the new public mint price
-     * Only callable by admin
+     * @dev Setter for the price to mint a token
+     * @param _newPrice the new price to mint a token
      */
     function setMintPrice(uint256 _newPrice) external onlyRole(ADMIN_ROLE) {
         price = _newPrice;
     }
 
     /**
-     * @dev Sets the max supply of the floth pass
-     * @param _newMaxSupply the new max supply
-     * Only callable by admin
+     * @dev Setter for the max supply of tokens
+     * @param _newMaxSupply the new max supply of tokens
      */
-    function setMaxSupply(uint16 _newMaxSupply) external onlyRole(ADMIN_ROLE){
+    function setMaxSupply(uint16 _newMaxSupply) external onlyRole(ADMIN_ROLE) {
         maxSupply = _newMaxSupply;
     }
 
     /**
-     * @dev Sets the ticker symbol of the contract
-     * @param _newSymbol the new ticker symbol
-     * Only callable by admin
+     * @dev Setter for the contract symbol
+     * @param _newSymbol the new symbol for the contract
      */
-    function setSymbol(
-        string calldata _newSymbol
-    ) external onlyRole(ADMIN_ROLE) {
-        ERC721AStorage.layout()._symbol = _newSymbol;
+    function setSymbol(string calldata _newSymbol) external onlyRole(ADMIN_ROLE) {
+        _setSymbol(_newSymbol);
     }
 
     /**
-     * @dev Sets the name of the contract
-     * @param _newName the new name of the contract
-     * Only callable by admin
+     * @dev Setter for the contract name
+     * @param _newName the new name for the contract
      */
     function setName(string calldata _newName) external onlyRole(ADMIN_ROLE) {
-        ERC721AStorage.layout()._name = _newName;
+        _setName(_newName);
     }
 
     /**
-     * @dev Sets the withdrawal address of the contract
-     * @param _withdrawAddress address to withdraw to
-     * Only callable by admin
+     * @dev Setter for the withdraw address
+     * @param _withdrawAddress the new withdraw address
      */
-    function setWithdrawAddress(
-        address payable _withdrawAddress
-    ) external onlyRole(ADMIN_ROLE) {
+    function setWithdrawAddress(address payable _withdrawAddress) external onlyRole(ADMIN_ROLE) {
+        if(_withdrawAddress == address(0)){
+            revert ZeroAddress();
+        }
         withdrawAddress = _withdrawAddress;
     }
 
     /**
-     * @dev Sets the floth contract address
-     * @param _flothContractAddress address of the floth contract
-     * Only callable by admin
+     * @dev Setter for the floth contract address
+     * @param _flothContractAddress the new floth contract address
      */
-    function setFlothContract(
-        address _flothContractAddress
-    ) external onlyRole(ADMIN_ROLE) {
+    function setFlothContract(address _flothContractAddress) external onlyRole(ADMIN_ROLE) {
         if(_flothContractAddress == address(0)){
-            revert CannotDeployAsZeroAddress();
+            revert ZeroAddress();
         }
-
         flothContract = IFloth(_flothContractAddress);
     }
 
     /**
-     * @dev Sets the base uri of the contract
-     * @param _baseUri base uri to change to
-     * Only callable by admin
+     * @dev Setter for the base uri
+     * @param _baseUri the new base uri
      */
-    function setBaseUri(
-        string calldata _baseUri
-    ) external onlyRole(ADMIN_ROLE) {
+    function setBaseUri(string calldata _baseUri) external onlyRole(ADMIN_ROLE) {
         _currentBaseURI = _baseUri;
     }
 
     /**
-     * @dev Overrides the base uri function in ERC721A
+     * @dev Internal function to get the base URI
+     * @return the base URI
      */
     function _baseURI() internal view override returns (string memory) {
         return _currentBaseURI;
     }
-    //#endregion
+
+    /**
+     * @dev Internal function to set the symbol
+     * @param newSymbol the new symbol
+     */
+    function _setSymbol(string memory newSymbol) internal {
+        bytes32 position = keccak256("eip1967.proxy.symbol");
+        assembly {
+            sstore(position, newSymbol)
+        }
+    }
+
+    /**
+     * @dev Internal function to set the name
+     * @param newName the new name
+     */
+    function _setName(string memory newName) internal {
+        bytes32 position = keccak256("eip1967.proxy.name");
+        assembly {
+            sstore(position, newName)
+        }
+    }
+
+    /**
+     * @dev Internal override for the before token transfer function
+     * This is to ensure that all logic is called before a token is transferred
+     * @param from the address to transfer from
+     * @param to the address to transfer to
+     * @param tokenId the token id
+     * @param batchSize the batch size
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    /**
+     * @dev Internal override for the after token transfer function
+     * This is to ensure that all logic is called after a token is transferred
+     * @param from the address to transfer from
+     * @param to the address to transfer to
+     * @param tokenId the token id
+     * @param batchSize the batch size
+     */
+    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721Upgradeable, ERC721VotesUpgradeable) {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
 }
