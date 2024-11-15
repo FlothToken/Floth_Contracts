7a370d3c203a59fc1c6d49fa9dc7104f4bd09f31
Westy <westy@ethereallabs.io>
2024-11-14 18:21:28 -0600

    :bulb: Removed warnings in ConsumerMock complication

--- contracts/FtsoV2ConsumerMock.sol
+++ contracts/FtsoV2ConsumerMock.sol
@@ -13,17 +13,17 @@ contract FtsoV2ConsumerMock {
         // Constructor logic removed as it's not needed for the mock
     }
 
-    function getFlrUsdPrice() public payable returns (uint256 value, int8 decimals, uint64 timestamp) {
+    function getFlrUsdPrice() public payable returns (uint256 /*value*/, int8 /*decimals*/, uint64 /*timestamp*/) {
         // Hardcoded return values
         return (50000000000000000000, 18, 1234567890);
     }
 
-    function getInputAmount(address pairAddress, address inputToken, uint256 outputAmount) public view returns (uint256 inputAmount) {
+    function getInputAmount(address /*pairAddress*/, address /*inputToken*/, uint256 /*outputAmount*/) public view returns (uint256 inputAmount) {
         // Hardcoded return value
         return 1000000000000000000; // 1 FLR
     }
 
-    function getDynamicPrice(uint256 usdAsk) external payable returns (uint256) {
+    function getDynamicPrice(uint256 /*usdAsk*/) external payable returns (uint256) {
         // Hardcoded return value
         return 2000000000000000000; // 2 FLR
     }
