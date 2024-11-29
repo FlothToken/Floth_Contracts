b59832ff9bbc979dab6eafe4c92eee82d8af0dc9
PxBenw <westy@ethereallabs.io>
2024-11-29 11:56:17 -0600

    🔥 Removed unnecessary function signatures

--- contracts/IFloth.sol
+++ contracts/IFloth.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.20;
 
 interface IFloth {
-    function getPastVotes(
-        address account,
-        uint256 timepoint
-    ) external view returns (uint256);
-
+    // Core ERC20 & voting functionality
+    function getPastVotes(address account, uint256 timepoint) external view returns (uint256);
     function delegate(address delegatee) external;
 
     function balanceOf(address account) external view returns (uint256);
@@ -14,13 +11,8 @@ interface IFloth {
     function getGrantFundWallet() external view returns (address);
 
     function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    
     function getTaxInfo() external view returns (
         uint128 buyTax,
         uint128 sellTax,
@@ -28,12 +20,7 @@ interface IFloth {
         bool paused
     );
     
-    function setSellTax(uint128 _newSellTax) external;
-    function setBuyTax(uint128 _newBuyTax) external;
-    function togglePause() external;
-    function addDexAddress(address _dexAddress) external;
-    function removeDexAddress(address _dexAddress) external;
-    function setLpTaxStatus(bool _status) external;
+    // Public view functions for addresses
     function dexAddresses(address) external view returns (bool);
     function lpFundWallet() external view returns (address);
 }
