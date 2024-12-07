b2fc527493a0192a88e85091b2b940d192435122
Westy <westy@ethereallabs.io>
2024-12-06 23:32:02 -0600

    :sparkles: Added recover erc20 function

--- contracts/pFloth.sol
+++ contracts/pFloth.sol
@@ -30,6 +30,7 @@ contract pFloth is ERC20, Ownable, ReentrancyGuard {
 
     // Mappings
     mapping(address => uint256) public pFLOTHBalance;
+    mapping(address => mapping(address => uint256)) public tokenSenders; // token => sender => amount
 
     /**
      * @dev Constructor for the pFloth contract
@@ -53,6 +54,7 @@ contract pFloth is ERC20, Ownable, ReentrancyGuard {
     );
     event TokenRecovered(address token, uint256 amount);
     event Withdraw(address owner, uint256 amount);
+    event TokenReturned(address indexed token, address indexed sender, uint256 amount);
 
     // Errors
     error PresaleEnded();
@@ -63,6 +65,7 @@ contract pFloth is ERC20, Ownable, ReentrancyGuard {
     error ExceedsWalletLimit();
     error InvalidRecoveryToken();
     error InvalidRecoveryAmount();
+    error UnauthorizedRecovery();
 
     // Modifiers
     modifier onlyDuringPresale() {
@@ -128,28 +131,44 @@ contract pFloth is ERC20, Ownable, ReentrancyGuard {
         emit Withdraw(msg.sender, _amount);
     }
 
-    //TODO: We need to be able to track who sent incorrect erc20s so we can send
-    // them back to the user. For example, wrapped flare might be sent.
-    
     /**
-     * @dev Recover any ERC20 tokens accidentally sent to the contract
-     * @param token The address of the token to recover
-     * @param amount The amount of tokens to recover
-     * Only the owner can call this function
-     * Cannot be used to recover pFLOTH tokens
-     * Implements nonReentrant pattern for security
-     * Emits a TokenRecovered event upon successful recovery
+     * @dev Override the token transfer hook to track incoming ERC20 tokens
+     * @param token The token being transferred
+     * @param sender The address sending the tokens
+     * @param amount The amount of tokens being transferred
      */
-    function recoverERC20(
+    function _beforeTokenTransfer(
         address token,
+        address sender,
         uint256 amount
-    ) external onlyOwner nonReentrant {
+    ) internal override {
+        if (token != address(this)) { // Only track non-pFLOTH tokens
+            tokenSenders[token][sender] += amount;
+        }
+    }
+
+    /**
+     * @dev Recover ERC20 tokens and return them to their original sender
+     * @param token The address of the token to recover
+     * @param sender The address that originally sent the tokens
+     * Anyone can recover their own tokens, owner can recover for others
+     * Cannot be used to recover pFLOTH tokens
+     */
+    function returnERC20ToSender(
+        address token,
+        address sender
+    ) external nonReentrant {
+        // Only allow msg.sender to recover their own tokens, or owner to recover for anyone
+        // TODO: Discuss the ramifications of this owner being able to recover for anyone.
+        if (msg.sender != sender && msg.sender != owner()) revert UnauthorizedRecovery();
         if (token == address(this)) revert InvalidRecoveryToken();
-        if (amount > IERC20(token).balanceOf(address(this))) 
-            revert InvalidRecoveryAmount();
         
-        IERC20(token).transfer(owner(), amount);
-        emit TokenRecovered(token, amount);
+        uint256 amount = tokenSenders[token][sender];
+        if (amount == 0) revert InvalidRecoveryAmount();
+        
+        tokenSenders[token][sender] = 0; // Reset the tracked amount
+        IERC20(token).transfer(sender, amount);
+        emit TokenReturned(token, sender, amount);
     }
 
     /**
